1d4291bb8640c0b5859ea691d1fb5eceb71d00db
Philip Bonev <philipb@acstre.com>
2026-08-02 14:27:00 +0300

    added magnet handling

--- src/lib.rs
+++ src/lib.rs
@@ -639,6 +639,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_single_instance::init(|app, argv, _cwd| {
             use tauri::Emitter;
             use tauri::Manager;
